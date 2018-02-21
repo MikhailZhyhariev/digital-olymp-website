49851d19c870a8c526fcb5928164bfcb69404b87
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-02-21 19:34:20 +0300

    Поправлены ошибки в отображении

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,7 +2,7 @@
   var carousel = document.querySelector('.carousel');
   var list = carousel.querySelector('ul');
 
-  if (window.innerWidth < 768) {
+  if (window.innerWidth < 568) {
     carousel.querySelector('#prev').onclick = function() {
         list.style.marginLeft = 0;
     }
