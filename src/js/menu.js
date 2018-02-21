945aa9d3595577484a02ceb80dda6117f188da22
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-02-21 18:55:20 +0300

    Добавлен адаптив для header. Исправлены баги отображения остальных элементов

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,21 +4,23 @@
   var button = menu.querySelector('.menu__button');
   var triangle = menu.querySelector('.menu__triangle');
 
-  var listCoord = list.getBoundingClientRect();
-  var buttonCoord = button.getBoundingClientRect();
-  var triangleCoord = triangle.getBoundingClientRect();
+  if (window.innerWidth < 1200) {
+    var listCoord = list.getBoundingClientRect();
+    var buttonCoord = button.getBoundingClientRect();
+    var triangleCoord = triangle.getBoundingClientRect();
 
-  list.style.display = 'none';
-  list.style.left = (buttonCoord.width - listCoord.width) / 2 + 'px';
-  list.style.top = 30 + 'px';
+    list.style.display = 'none';
+    list.style.left = (buttonCoord.width - listCoord.width) / 2 + 'px';
+    list.style.top = 30 + 'px';
 
-  triangle.style.left = (listCoord.width - triangleCoord.width) / 2 + 'px';
+    triangle.style.left = (listCoord.width - triangleCoord.width) / 2 + 'px';
 
-  button.onclick = function() {
-    if (list.style.display == 'none') {
-      list.style.display = 'block';
-    } else {
-      list.style.display = 'none';
-    }
-  };
+    button.onclick = function() {
+      if (list.style.display == 'none') {
+        list.style.display = 'block';
+      } else {
+        list.style.display = 'none';
+      }
+    };
+  }
 })();
